6d6af7c560fa15f6c878d1f3a6bfd21cde07cf05
Matt Fellows <matt.fellows@onegeek.com.au>
2017-07-19 09:26:56 +1000

    chore(tests): exclude vendor in tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,7 +13,7 @@ go get github.com/modocache/gover
 echo "mode: count" > profile.cov
 
 # Standard go tooling behavior is to ignore dirs with leading underscors
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path 'vendor/*' -not -path '*/_*' -type d); do
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path './vendor/*' -not -path '*/_*' -type d); do
   if ls $dir/*.go &> /dev/null; then
     go test -covermode=count -coverprofile=$dir/profile.tmp $dir
     if [ -f $dir/profile.tmp ]; then
